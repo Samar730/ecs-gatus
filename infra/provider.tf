3c7d6cfc8354b8373381c04492a7b4dd1d315a32
Samar730 <samar.akbari1@gmail.com>
2026-03-27 20:57:18 +0000

    Add provider and backend for infra & S3 remote backend with lockfile

---
+++ infra/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket       = "ecs-gatus-tfstate"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
